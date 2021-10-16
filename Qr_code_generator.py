549131534d75db26335cddab769c63b90c43951c
sgsayani <71175346+sgsayani@users.noreply.github.com>
2021-10-16 08:51:23 +0530

    Update Qr_code_generator.py

--- Qr_code_generator.py
+++ Qr_code_generator.py
@@ -5,7 +5,7 @@ qr=qrcode.QRCode(
     border=6
     )
 
-data="Hello everyone! I am a qrcode"
+data="Hello everyone! I am a qrcode" #if you want to add any link you can use 'link'
 qr.add_data(data)
 qr.make(fit=True)
 img=qr.make_image(fill="black",back_color="white")
